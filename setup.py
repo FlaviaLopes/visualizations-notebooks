d1e91a2a2b6b1364e37e7f9a80469975cf03ad4c
FlaviaLopes <flavithalopes@hotmail.com>
2022-06-02 01:50:26 -0300

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+setup(
+    name='VisualizationsNotebooks',
+    packages=find_packages(include=['VisualizationsNotebooks']),
+    version='0.0.1',
+    description='Some visualizations tests',
+    author='Flávia Lopes',
+    license='MIT',
+)
